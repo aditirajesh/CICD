7bda8c1dc4624ee7db0261d25bb51b79bc2b3b31
Aditi Rajesh <aditir2607@gmail.com>
2025-12-03 12:47:09 +0530

    adding terraform files

---
+++ day1/main.tf
@@ -0,0 +1,8 @@
+    resource "aws_s3_bucket" "my_bucket" {
+      bucket = "aditi-s3-bucket-jenkins" # Replace with a globally unique name
+      tags = {
+        Environment = "Development"
+        Project     = "MyProject"
+        Owner = "arajesh"
+      }
+    }
